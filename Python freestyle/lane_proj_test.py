7cca8a71dad89f169850ca9332c6d7ef586e52a4
Hussain-D <dudekulahussain86@gmail.com>
2023-01-10 22:44:09 +0530

    initial work

---
+++ Python freestyle/lane_proj_test.py
@@ -0,0 +1,30 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# CONVERT INTO GREY SCALE IMAGE
+def grey(image):
+    image = np.asarray(image)
+    return cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+
+# GAUSSIAN BLUR TO REDUCE NOISE AND SMOOTHEN THE IMAGE
+def gauss(image):
+    return cv2.GaussianBlur(image, (5, 5), 0)
+
+
+# CANNY EDGE DETECTION
+def canny(image):
+    edges = cv2.Canny(image, 100, 200)
+    return edges
+
+
+# IMPORTING IMAGE
+img = cv2.imread("C:/Users/D P Balaiah/Documents/Sem 7/Major Project/Hu/sample lane image.jpg")
+
+# GREYSCALING
+grey_img = grey(img)
+plt.imshow(grey_img)
+
+plt.show()
