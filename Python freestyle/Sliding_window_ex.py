1f60c62c2ae76f66202974148c093c17575ed77a
Hussain-D <dudekulahussain86@gmail.com>
2023-08-22 17:36:11 +0530

    ignorant error

--- Python freestyle/Sliding_window_ex.py
+++ Python freestyle/Sliding_window_ex.py
@@ -3,7 +3,7 @@
     Input  : arr[] = {100, 200, 300, 400}, k = 2
     Output : 700
 
-    Input  : arr[] = {1, 4, 2, 10, 23, 3, 1, 0, 20}, k = 4 
+    Input  : arr[] = {1, 4, 2, 10, 23, 3, 1, 0, 20}, k = 4
     Output : 39
     We get maximum sum by adding subarray {4, 2, 10, 23} of size 4.
 
