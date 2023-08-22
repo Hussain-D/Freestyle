4ac139caae29d53f7d00f8dc5dc1fa091f9a0fc7
Hussain-D <dudekulahussain86@gmail.com>
2023-08-22 17:25:17 +0530

    use of map, filter

--- Python freestyle/map_filter_ex.py
+++ Python freestyle/map_filter_ex.py
@@ -2,10 +2,15 @@ list_1 = [1,2,3,4,5]
 
 double_list = list(map(lambda i:i*2,list_1))
 
-print(list_1)
-print(double_list)
+print("Initial list: ", list_1)
+print("List of doubled numbers: ",double_list)
 
 filter_evens = list(filter(lambda i:i%2==0,list_1))
 
-print(list_1)
-print(filter_evens)
+print("Initial list: ",list_1)
+print("List of even numbers: ",filter_evens)
+
+filter_odds = list(filter(lambda i:i%2!=0,list_1))
+
+print("Initial list: ",list_1)
+print("List of odd numbers: ",filter_odds)
