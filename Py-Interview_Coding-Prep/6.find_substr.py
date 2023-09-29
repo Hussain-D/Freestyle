c98f1c062c8cf1dcfb9d80a35d1f85555bb134a1
Hussain-D <dudekulahussain86@gmail.com>
2023-09-29 13:01:24 +0530

    Sliding window

---
+++ Py-Interview_Coding-Prep/6.find_substr.py
@@ -0,0 +1,16 @@
+"""
+Find the position of the substring in the main string
+"""
+s1 = 'abcd'
+s2 = 'kjkeoldlabcdadfdabcdv'
+ls1 = len(s1)
+ls2 = len(s2)
+
+if ls1>ls2:
+    for i in range(ls1-ls2+1):
+        if s1[i:i+ls2] == s2:
+            print('At ',i,'Till',i+ls2-1)
+else:
+    for i in range(ls2-ls1+1):
+        if s2[i:i+ls1] == s1:
+            print('At ',i,'Till',i+ls1-1)
